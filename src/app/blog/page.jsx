8951f12c4d9d491738c8f34749e3046d3558ff86
Juan Bagnasco <jachiappara@gmail.com>
2024-01-06 21:10:40 -0300

    Empece con las cards del blogPage

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,3 +1,26 @@
+import PostCard from "@/ui/postCard/postcard";
+
 export default function BlogPage() {
-  return <div>BlogPage</div>;
+  const cards = [
+    {
+      title: "Travel",
+      description: "Two weeks in the south of Spain",
+      hrefImg:
+        "https://images.pexels.com/photos/16794803/pexels-photo-16794803/free-photo-of-rocas-muelle-fachada-litoral.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      date: "25.05.2021",
+      hrefMore:
+        "https://www.pexels.com/es-es/foto/rocas-muelle-fachada-litoral-16794803/",
+    },
+    { title: "", description: "", hrefImg: "", date: "", hrefMore: "" },
+    { title: "", description: "", hrefImg: "", date: "", hrefMore: "" },
+    { title: "", description: "", hrefImg: "", date: "", hrefMore: "" },
+  ];
+
+  return (
+    <div className="w-full flex justify-between flex-wrap">
+      {cards.map((card) => (
+        <PostCard post={card} />
+      ))}
+    </div>
+  );
 }
--- src/ui/postCard/postcard.jsx
+++ src/ui/postCard/postcard.jsx
@@ -1,3 +1,38 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function PostCard({ post }) {
-  return <div></div>;
+  const styles = {
+    card: "w-3/12 mx-1",
+    header: "flex justify-between items-center",
+    imgDiv: "relative w-full h-96",
+    img: "overflow-hidden",
+    date: "text-md text-gray-400 font-semibold -mx-2 transform -rotate-90",
+    title: "",
+    description: "",
+    link: "",
+  };
+
+  return (
+    <div className={styles.card}>
+      <header className={styles.header}>
+        <div className={styles.imgDiv}>
+          <Image
+            className={styles.img}
+            src={post.hrefImg}
+            fill
+            style={{ objectFit: "cover" }}
+          />
+        </div>
+        <span className={styles.date}>{post.date}</span>
+      </header>
+      <div>
+        <h3 className={styles.title}>{post.title}</h3>
+        <p className={styles.description}>{post.description}</p>
+        <Link className={styles.link} href={`/blog/${""}`}>
+          Read More
+        </Link>
+      </div>
+    </div>
+  );
 }
